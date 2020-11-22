d95cf68276ca899e19c7ceb963be7239188aae11
Alexander <alexanderhaberling@yahoo.de>
2020-11-22 16:27:00 +0100

    GraphTool_explo.py transfer

--- debateDB_export.py
+++ debateDB_export.py
@@ -7,7 +7,9 @@ def extrac_complete(tx):
     tx.run("""CALL apoc.export.graphml.all('debate.org_full.graphml', {})""")
 
 def extrac_friends(tx):
-    tx.run("""MATCH (a:User) MATCH (n:User)-[r:FRIENDS_WITH]-(m:User) WITH n AS nodes, r AS relations CALL apoc.export.graphml.data(nodes, relations, 'debate.org_friends.graphml', {})""")
+    #tx.run("""MATCH (a:User) WITH a AS users MATCH (n:User)-[r:FRIENDS_WITH]-(m:User) WITH r AS friends CALL apoc.export.graphml.data(users, friends, 'debate.org_friends.graphml', {}) YIELD file, source, format, nodes, relationships, properties, time, rows, batchSize, batches, done, data RETURN file, source, format, nodes, relationships, properties, time, rows, batchSize, batches, done, data""")
+    #tx.run("""MATCH (a:User) WITH collect(a) AS users CALL apoc.export.graphml.data(users, [], 'debate.org_friends.graphml', {}) YIELD file, source, format, nodes, relationships, properties, time, rows, batchSize, batches, done, data RETURN file, source, format, nodes, relationships, properties, time, rows, batchSize, batches, done, data""")
+    tx.run("""MATCH (n:User)-[r:FRIENDS_WITH]-(m:User) WITH collect(n) AS users, collect(r) AS friendships CALL apoc.export.graphml.data(users, friendships, 'debate.org_friends.graphml', {}) YIELD file, source, format, nodes, relationships, properties, time, rows, batchSize, batches, done, data RETURN file, source, format, nodes, relationships, properties, time, rows, batchSize, batches, done, data""")
 
 
 with driver.session() as session:
