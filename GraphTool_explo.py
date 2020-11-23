4c281b715fb0ce3e0725e2b8363d71431057cb21
Alexander <alexanderhaberling@yahoo.de>
2020-11-23 16:55:31 +0100

    GraphTool_explo.py export/import functional

--- GraphTool_explo.py
+++ GraphTool_explo.py
@@ -14,7 +14,7 @@ e = g.add_edge(v1, v2)
 graph_draw(g, vertex_text=g.vertex_index, output="two-nodes.pdf")
 
 print(v1.out_degree())
-print(e.source(), e.target())
+print(e.source(), e.target(), "hahaha lalala")
 print(e.source(), e.target())
 
 vlist = g.add_vertex(10)
@@ -26,4 +26,17 @@ v = g.add_vertex()
 print(g.vertex_index[v])
 
 print(int(v))
+
+g_friendship = load_graph('debate.org_friends_withType_limit_mod.graphml', fmt='graphml')
+
+print(g_friendship)
+
+print(g.vertex_index[12])
+'''
+for v in g_friendship.vertices():
+    print(v)
+for e in g_friendship.edges():
+    print(e)
+'''
+#graph_draw(g_friendship, vertex_text=g.vertex_index, output="g_friendship.pdf")
 
