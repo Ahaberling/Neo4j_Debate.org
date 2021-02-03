55bf40473456ec273894cc71a438f5fb6b50b96a
Alexander Haberling <alexanderhaberling@yahoo.de>
2021-02-03 15:24:32 +0100

    manual git commit

--- debateDB_Exportation.py
+++ debateDB_Exportation.py
@@ -1,7 +1,17 @@
 from neo4j import GraphDatabase
 
+######################
+### Initialization ###
+######################
+
 driver = GraphDatabase.driver("neo4j://localhost:7687", auth=("neo4j", "123"))
 
+
+##########################
+### Functions: Extract ###
+##########################
+
+# Complete Database
 def extrac_complete(tx):
     tx.run("""CALL apoc.export.graphml.all('debate.org_full.graphml', {})""")
 
@@ -9,7 +19,14 @@ def extrac_complete(tx):
 def extrac_friends(tx):
     tx.run("""MATCH (n: User)-[r1:GIVES_ISSUES]-(m:Issues) OPTIONAL MATCH (n:User)-[r2:FRIENDS_WITH]-() WITH COLLECT(DISTINCT n) AS users, COLLECT(DISTINCT m) AS issues, COLLECT(r1) AS gives_issues, COLLECT(r2) AS firends_with CALL apoc.export.graphml.data(users + issues, gives_issues + firends_with, 'debate.org_with_issues.graphml', {useTypes:true}) YIELD file, source, format, nodes, relationships, properties, time, rows, batchSize, batches, done, data RETURN file, source, format, nodes, relationships, properties, time, rows, batchSize, batches, done, data""")
 
+
+###############
+### Session ###
+###############
+
 with driver.session() as session:
 
     #session.write_transaction(extrac_complete)
     session.write_transaction(extrac_friends)
+
+print("\ndebateDB_Exportation.py - done\n")
